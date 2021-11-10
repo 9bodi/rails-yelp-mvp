99a536a885fb72dcfcb8b33f6d59f720197c64d4
nicolas bodineau <bodineau.nico@gmail.com>
2021-11-10 16:50:34 +0100

    done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  def new_review
+    # we need @restaurant in our `simple_form_for`
+    @review = Review.new(review_params)
+  end
+
+  def create_review
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def destroy_review
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
